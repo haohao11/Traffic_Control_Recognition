d29d93d5f7948a87ba863db14b10a1565d93a82c
hao_p53 <chenghao605@gmail.com>
2020-07-03 18:30:03 +0200

    add cvae model

---
+++ scripts/model.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 16:59:10 2020
+
+@author: cheng
+"""
+
+
+# from keras_multi_head import MultiHeadAttention
+
+from keras.layers import Input, Dense, Lambda, concatenate, LSTM, Activation, Flatten, MaxPooling2D
+from keras.layers.convolutional import Conv2D, Conv1D
+from keras.models import Model
+from keras import backend as K
+from keras.layers.core import RepeatVector, Dropout
+from keras.layers.wrappers import TimeDistributed
+from keras import optimizers
+from keras.losses import mse, CategoricalCrossentropy 
+#CategoricalCrossentropy
+
+
+class CVAE():
+    
+    def __init__(self, args):
+        # Store the hyperparameters
+        self.args = args
+        self.window_size = args.window_size
+        self.num_features = args.num_features
+        self.num_classes = args.num_classes
+        self.hidden_size = args.hidden_size
+        self.z_dim = args.z_dim
+        self.encoder_dim = args.encoder_dim
+        self.z_decoder_dim = args.z_decoder_dim
+        self.s_drop = args.s_drop
+        self.z_drop = args.z_drop
+        self.lr = args.lr
+        self.beta = args.beta      
+        
+        #################### MODEL CONSTRUCTION STARTS FROM HERE ####################
+        # Construct the condition model
+        self.x = Input(shape=(self.window_size, self.num_features), name='x') 
+        self.x_conv1d = Conv1D(self.hidden_size//2, kernel_size=3, strides=1, padding='same', name='x_conv1d')(self.x)
+        self.x_dense = Dense(self.hidden_size, activation='relu', name='x_dense')(self.x_conv1d)
+        self.x_state = LSTM(self.encoder_dim,
+                       return_sequences=False,
+                       stateful=False,
+                       dropout=self.s_drop,
+                       name='x_state')(self.x_dense)
+        
+        # Construct the label model
+        self.y = Input(shape=(self.window_size, self.num_classes), name='y')
+        self.y_conv1d = Conv1D(self.hidden_size//2, kernel_size=3, strides=1, padding='same', name='y_conv1d')(self.y)
+        self.y_dense = Dense(self.hidden_size, activation='relu', name='y_dense')(self.y_conv1d)
+        self.y_state = LSTM(self.encoder_dim,
+                       return_sequences=False,
+                       stateful=False,
+                       dropout=self.s_drop,
+                       name='y_state')(self.y_dense)
+        
+        # CONSTRUCT THE CVAE ENCODER BY FEEDING THE CONCATENATED X AND Y
+        # the concatenated input
+        self.inputs = concatenate([self.x_state, self.y_state], name='inputs') 
+        self.xy_encoded_d1 = Dense(self.hidden_size, activation='relu', name='xy_encoded_d1')(self.inputs) 
+        self.xy_encoded_d2 = Dense(self.hidden_size//2, activation='relu', name='xy_encoded_d2')(self.xy_encoded_d1)
+        self.mu = Dense(self.z_dim, activation='linear', name='mu')(self.xy_encoded_d2) # 2
+        self.log_var = Dense(self.z_dim, activation='linear', name='log_var')(self.xy_encoded_d2) # 2
+        
+        
+        # THE REPARAMETERIZATION TRICK FOR THE LATENT VARIABLE z
+        # sampling function
+        z_dim = self.z_dim
+        def sampling(params):
+            mu, log_var = params
+            eps = K.random_normal(shape=(K.shape(mu)[0], z_dim), mean=0., stddev=1.0)
+            return mu + K.exp(log_var/2.) * eps
+        
+        # sampling z
+        self.z = Lambda(sampling, output_shape=(self.z_dim,), name='z')([self.mu, self.log_var])
+        # concatenate the z and x_encoded_dense
+        self.z_cond = concatenate([self.z, self.x_state], name='z_cond')
+            
+        # CONSTRUCT THE CVAE DECODER
+        self.z_decoder1 = Dense(self.hidden_size//2, activation='relu', name='z_decoder1')
+        self.z_decoder2 = RepeatVector(self.window_size, name='z_decoder2')
+        self.z_decoder3 = LSTM(self.z_decoder_dim,
+                          return_sequences=True,
+                          stateful=False,
+                          dropout=self.z_drop,
+                          name='z_decoder3')
+        self.z_decoder4 = Activation('tanh', name='z_decoder4')
+        self.z_decoder5 = Dropout(self.z_drop, name='z_decoder5')
+        self.y_decoder = TimeDistributed(Dense(self.num_classes, activation='softmax'), name='y_decoder') # 
+        
+        # Instantiate the decoder by feeding the concatenated z and x_encoded_dense
+        # Reconstrcting y
+        self.z_d1 = self.z_decoder1(self.z_cond)
+        self.z_d2 = self.z_decoder2(self.z_d1)
+        self.z_d3 = self.z_decoder3(self.z_d2)
+        self.z_d4 = self.z_decoder4(self.z_d3)
+        self.z_d5 = self.z_decoder5(self.z_d4)
+        self.y_prime = self.y_decoder(self.z_d5)
+        
+        
+    def training(self):
+        """
+        Construct the CVAE model in training time
+        Both features and class label are available 
+        y is the ground truth class label
+        """
+        print('Contruct the cvae model for training')
+        
+        def vae_loss(y, y_prime):
+            '''
+            This is the customized loss function
+            It consists of crossentropy loss and KL loss
+            '''
+            # reconstruction_loss = K.mean(mse(y, self.y_prime)*self.window_size)
+            reconstruction_loss = K.mean(K.categorical_crossentropy(y, self.y_prime))
+            kl_loss = 0.5 * K.sum(K.square(self.mu) + K.exp(self.log_var) - self.log_var - 1, axis=-1)
+            cvae_loss = K.mean(reconstruction_loss*self.beta + kl_loss*(1-self.beta))
+            return cvae_loss
+        
+        # BUILD THE CVAE MODEL
+        cvae = Model([self.x, self.y], 
+                     [self.y_prime])
+        opt = optimizers.Adam(lr=self.lr, beta_1=0.9, beta_2=0.999, decay=1e-6, amsgrad=False)
+        cvae.compile(optimizer=opt, loss=vae_loss)
+        return cvae
+    
+    
+    def X_encoder(self):
+        """
+        Construct the encoder to get the x_encoded_dense, 
+        NOTE: 
+            In inference phase, ONLY features are availabel
+        Returns
+        x_encoder : TYPE
+            DESCRIPTION.
+        """
+        print('Construct the X-Encoder for inference')            
+        x_encoder = Model(self.x, self.x_state)
+        # x_encoder.summary()
+        return x_encoder
+    
+    
+    def Decoder(self):           
+        # CONSTRUCT THE DECODER
+        print('Construct the Decoder for trajectory oreidction')
+        decoder_input = Input(shape=(self.z_dim+self.encoder_dim, ), name='decoder_input')
+        _z_d1 = self.z_decoder1(decoder_input)
+        _z_d2 = self.z_decoder2(_z_d1)
+        _z_d3 = self.z_decoder3(_z_d2)
+        _z_d4 = self.z_decoder4(_z_d3)
+        _z_d5 = self.z_decoder5(_z_d4)
+        _y_prime = self.y_decoder(_z_d5)
+        generator = Model(decoder_input, _y_prime)
+        # generator.summary()
+        return generator
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
--- scripts/trainer.py
+++ scripts/trainer.py
@@ -14,8 +14,10 @@ import numpy as np
 import os
 import sys
 
-from read_junc import preprocess
 from data_utils import Load_data
+from read_junc import preprocess
+from model import CVAE
+
 
 np.set_printoptions(suppress=True)
 
@@ -33,7 +35,42 @@ def parse_args():
     parser.add_argument('--window_size', type=int, default=8, 
                         help='sequence length for the sliding window')
     parser.add_argument('--stride', type=int, default=8, 
-                        help='stride for the sliding window')                           
+                        help='stride for the sliding window') 
+    parser.add_argument('--num_features', type=int, default=7, 
+                        help='number of input features')
+    parser.add_argument('--num_classes', type=int, default=5, 
+                        help='number of input classes')
+
+    parser.add_argument('--encoder_dim', type=int, default=16, 
+                        help='This is the size of the encoder output dimension')
+    parser.add_argument('--z_dim', type=int, default=2, 
+                        help='This is the size of the z dimension')
+    parser.add_argument('--z_decoder_dim', type=int, default=64, 
+                        help='This is the size of the decoder LSTM dimension')
+    parser.add_argument('--hidden_size', type=int, default=32, 
+                        help='The size of GRU hidden state')
+    parser.add_argument('--batch_size', type=int, default=352, help='Batch size')
+    parser.add_argument('--s_drop', type=float, default=0.1, 
+                        help='The dropout rate for trajectory sequence')
+    parser.add_argument('--z_drop', type=float, default=0.2, 
+                        help='The dropout rate for z input')
+    parser.add_argument('--beta', type=float, default=0.75, 
+                        help='Loss weight')   
+    parser.add_argument('--train_mode', type=bool, default=True, 
+                        help='This is the training mode')
+    parser.add_argument('--split', type=float, default=0.8, 
+                        help='the split rate for training and validation')
+    parser.add_argument('--lr', type=float, default=1e-3, 
+                        help='Learning rate')
+    parser.add_argument('--aug_num', type=int, default=8, 
+                        help='Number of augmentations')
+    parser.add_argument('--epochs', type=int, default=100, 
+                        help='Number of batches')
+    parser.add_argument('--patience', type=int, default=5, 
+                        help='Maximum mumber of continuous epochs without converging')    
+
+
+                          
     args = parser.parse_args(sys.argv[1:])
     return args
     
@@ -70,9 +107,24 @@ def main():
         print(sequence_feature)
         print(sequence)
         
+        r = sequence_feature[:, -1] / sequence_feature[:, -2]
+        print(r)
+        
         count += 1       
         if count == 1:
             break
+        
+    # # Instantiate the model
+    cvae = CVAE(args)   
+    # Contruct the cave model    
+    train =cvae.training() 
+    train.summary() 
+    
+    x_encoder=cvae.X_encoder()
+    decoder = cvae.Decoder()       
+    x_encoder.summary()
+    decoder.summary()
+    
 
 
 
